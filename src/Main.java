ae13500180e05ca6a1a12bbdfaa1dac7f0660ec2
WENDELL DE SOUZA <wendell.souza8@senaisp.edu.br>
2026-08-28 17:03:46 -0300

    Removendo duplicidade do metodo main

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    Scanner scanner = new Scanner(System.in);
-    conta conta = new conta();
-    double valor = 0;
 
-    conta.setValor(valor);
-    conta.setTitular("Pablo");
-    IO.println("Ola "+conta.getTitular());
-    IO.println("Deseja: ");
-    IO.println("Saldo "+conta.getSaldo());
-    IO.println("(1) Sacar");
-    IO.println("(2) Depositar");
-    int decisao = scanner.nextInt();
+import java.util.Scanner;
 
-    if (decisao == 1){
-        IO.println("Deseja sacar quanto: ");
-        IO.println("(1)R$10,00");
-        IO.println("(2)R$50,00");
-        IO.println("(3)R$100,00");
-        int saque = scanner.nextInt();
-       if(saque == 1){
-            valor += 5;
+public class Main {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            conta conta = new conta();
+            double valor = 0;
+
             conta.setValor(valor);
-            conta.sacar();
-        }else if (saque ==  2){
-            valor += 50;
-            conta.setValor(valor);
-            conta.sacar();
-        }else if (saque == 3){
-            valor += 100;
-            conta.setValor(valor);
-            conta.sacar();
+            conta.setTitular("Pablo");
+            IO.println("Ola " + conta.getTitular());
+            IO.println("Deseja: ");
+            IO.println("Saldo " + conta.getSaldo());
+            IO.println("(1) Sacar");
+            IO.println("(2) Depositar");
+            int decisao = scanner.nextInt();
+
+            if (decisao == 1) {
+                IO.println("Deseja sacar quanto: ");
+                IO.println("(1)R$10,00");
+                IO.println("(2)R$50,00");
+                IO.println("(3)R$100,00");
+                int saque = scanner.nextInt();
+                if (saque == 1) {
+                    valor += 5;
+                    conta.setValor(valor);
+                    conta.sacar();
+                } else if (saque == 2) {
+                    valor += 50;
+                    conta.setValor(valor);
+                    conta.sacar();
+                } else if (saque == 3) {
+                    valor += 100;
+                    conta.setValor(valor);
+                    conta.sacar();
+                }
+
+            } else if (decisao == 2) {
+                IO.println(conta.depositar());
+                IO.println("Deseja depositar quanto: ");
+                IO.println("(1)R$10,00");
+                IO.println("(2)R$50,00");
+                IO.println("(3)R$100,00");
+
+            }
+
+
         }
-
-    }else if(decisao == 2){
-        IO.println(conta.depositar());
-        IO.println("Deseja depositar quanto: ");
-        IO.println("(1)R$10,00");
-        IO.println("(2)R$50,00");
-        IO.println("(3)R$100,00");
-
     }
-
-
-}
